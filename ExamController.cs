a9043b03f28c1be7d03332b5a21a8f340f5957cb
ShubzA <142892995+ShubzA@users.noreply.github.com>
2023-08-22 16:24:07 +0530

    Add files via upload

---
+++ ExamController.cs
@@ -0,0 +1,74 @@
+﻿using demopopway.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demopopway.Controllers
+{
+    
+    public class ExamController : Controller
+    {
+        demoappEntities3 ExamDb = new demoappEntities3();
+        // GET: Exam
+        public ActionResult scoreinfo(Exam__Score exob)
+        {
+            return View(exob);
+        }
+
+        [HttpPost]
+        public ActionResult AddScore(Exam__Score model)
+        {
+             
+            Exam__Score exsobj = new Exam__Score();
+            //if (ModelState.IsValid)
+            //{
+                exsobj.id = model.id;
+                exsobj.subject = model.subject;
+                exsobj.score = model.score;
+                exsobj.student_id = model.student_id; 
+
+            
+
+                if (model.id == 0)
+                {
+                    
+                ExamDb.Exam__Score.Add(exsobj); 
+                ExamDb.SaveChanges();
+
+                }
+                else
+                {
+                    ExamDb.Entry(exsobj).State = EntityState.Modified;
+                    ExamDb.SaveChanges();
+                }
+
+
+            //}
+            ModelState.Clear();
+            return View("scoreinfo");
+        }
+
+        [HttpGet]
+        public ActionResult GetScore(Exam__Score model)
+        {
+            using(demoappEntities3 db =new demoappEntities3())
+            {
+                var res = db.Exam__Score.Include("Student").ToList();
+                return View(res);
+            }
+            
+        }
+        public ActionResult delete(int id)
+        {
+            var res = ExamDb.Exam__Score.Where(x => x.id == id).First();
+            ExamDb.Exam__Score.Remove(res);
+            ExamDb.SaveChanges();
+
+            var list = ExamDb.Exam__Score.ToList();
+            return View("GetScore", list);
+        }
+    }
+}
---
+++ StudentController.cs
@@ -0,0 +1,72 @@
+﻿using demopopway.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demopopway.Controllers
+{
+    public class StudentController : Controller
+    {
+        demoappEntities3 StudentDb = new demoappEntities3();
+        
+        // GET: Student
+        public ActionResult StudentInfo(Student stob)
+
+        {
+            
+                return View(stob);
+             
+        }
+
+        [HttpPost]
+        public ActionResult AddStudent(Student model)
+        {
+            Student stobj = new Student();
+            if (ModelState.IsValid)
+            {
+                stobj.ID = model.ID;
+                stobj.Student_Name = model.Student_Name;
+                stobj.Student_Age = model.Student_Age;
+                stobj.Contact_Number = model.Contact_Number;
+                stobj.Address = model.Address;
+                if(model.ID ==0)
+                {
+                    StudentDb.Students.Add(stobj);
+                    StudentDb.SaveChanges();
+
+                }
+                else
+                {
+                    StudentDb.Entry(stobj).State = EntityState.Modified;
+                    StudentDb.SaveChanges();
+                }
+
+               
+            }
+            ModelState.Clear();
+            return View("StudentInfo");
+        }
+
+        [HttpGet]
+        public ActionResult GetStudent(Student model)
+        {
+            var res = StudentDb.Students.ToList();
+            return View(res);
+        }
+        public ActionResult delete(int id)
+        {
+            var res = StudentDb.Students.Where(x => x.ID == id).First();
+            StudentDb.Students.Remove(res);
+            StudentDb.SaveChanges();
+
+            var list = StudentDb.Students.ToList();
+            return View("GetStudent",list);
+        }
+
+
+        
+    }
+}
